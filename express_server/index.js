0dc84e01fea7944333191a10a8b8e662f22d665d
Said Mohamad Ammar <mohammadammar130@gmail.com>
2022-10-26 15:02:40 +0200

    organise server

--- express_server/index.js
+++ express_server/index.js
@@ -6,6 +6,9 @@ const server = new WebSocketServer({ port: 3000 });
 const PC_type = "PC";
 const RASP_type = "RASP";
 
+// a list that contains the remote devices that are connected to server
+server.remote_devices_connected_list = [];
+
 const devices = ["pc_123", "rasp_123", "PC_1", "ras_1"];
 
 console.log("Server listening at 3000");
@@ -56,12 +59,9 @@ function deal_connection(socket, data){
 
   if((socket_type == PC_type)){
     if ((devices.includes(client_id) ) && (devices.includes(remote_device_id))){   
-      if (!is_remote_device_busy(remote_device_id)){
+      if (!is_remote_device_busy(server, remote_device_id)){
         
-        socket.send(JSON.stringify({
-          type: "connect",
-          answer : "accepted"
-        }));
+        socket.send(json_answer_connection(true));
         socket.client_id = client_id;
         socket.remote_device_id = remote_device_id;
         socket.client_type = socket_type
@@ -76,12 +76,11 @@ function deal_connection(socket, data){
 
   if ((socket_type == RASP_type)){
     if (devices.includes(remote_device_id)){
-    socket.send(JSON.stringify({
-      type: "connect",
-      answer : "accepted"
-    }));
+    socket.send(json_answer_connection(true));
     socket.remote_device_id = remote_device_id;
-    socket.client_type = socket_type
+    socket.client_type = socket_type;
+    // update that remote device is connected 
+    remote_device_connected(server, remote_device_id);
     console.log("accepted");
     } else{
     reject_connection(socket, "Error in id");
@@ -95,25 +94,66 @@ function deal_connection(socket, data){
  * @param {*} error  error to be sent
  */
 function reject_connection(socket, error){
-  socket.send(JSON.stringify({
-    type: "connect",
-    answer : "rejected",
-    message : error
-  }));
+  socket.send(json_answer_connection(false, error));
   console.log("rejected");
   socket.close();
 }
+
+/*
+-------------------------
+Remote device part 
+-------------------------
+*/
 
 /**
  * Check if remote_device (raspberry) is already linked to a client (PC)
  * @param {*} remote_device_id : the id of the remote_device
  * @returns true if remote_device is connected to a PC, false if not
  */
-function is_remote_device_busy(remote_device_id){
+function is_remote_device_busy(server, remote_device_id){
   for(let client of server.clients){
-    if ((client.client_type == "PC") && (client.remote_device_id == remote_device_id)){
+    if ((client.client_type == PC_type) && (client.remote_device_id == remote_device_id)){
       return true;
     }
   }
   return false; 
+}
+
+
+
+function remote_device_connected(server, remote_device_id){
+  server.remote_devices_connected_list.push(remote_device_id);
+  for(let client of server.clients){
+    if ((client.client_type == PC_type) && (client.remote_device_id == remote_device_id)){
+      break;
+    }
+  }
+}
+
+
+/*
+------------------------------------------
+Part of JSON message to send to client
+------------------------------------------
+*/
+
+/**
+ * This function returns a JSON string that indicate if the connection is accpeted or not
+ * @param {*} accepted boolean to indicate if connection is accepted or not
+ * @param {*} message message in case if connection is srejected
+ * @returns 
+ */
+function json_answer_connection(accepted, message = ""){
+  if(accepted){
+    return JSON.stringify({
+      type: "connect",
+      answer : "accepted"
+    })
+  } else{
+    return JSON.stringify({
+      type: "connect",
+      answer : "rejected",
+      message : message
+    })
+  }
 }
