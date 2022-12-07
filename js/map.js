838ce1b2c1f2bb3292eae80c0413165532fd1ffa
Said Mohamad Ammar <mohammadammar130@gmail.com>
2022-12-07 13:54:44 +0100

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,10 +1,11 @@
+var map;
 // Initialize and add the map
 function initMap() {
     console.log("Hello");
     // The location of Uluru
     const uluru = { lat: 48.715861, lng: 2.211261 };
     // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
+    map = new google.maps.Map(document.getElementById("map"), {
       zoom: 16,
       center: uluru,
     });
@@ -13,6 +14,20 @@ function initMap() {
       position: uluru,
       map: map,
     });
+
+    marker.addListener('click', test_mark);
+    map.addListener('click', test_mark);
   }
   
   window.initMap = initMap;
+
+  function test_mark(ele){
+    console.log(ele);
+    console.log(ele.latLng.lat());
+    console.log(ele.latLng.lng());
+    const marker = new google.maps.Marker({
+      // position: {lat:ele.latLng.lat(), },
+      map: map,
+    });
+    console.log("Marker clicked");
+  }
